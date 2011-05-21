b4b1008c0e4dd76c4f33f2c5de17c032188dcb0a
Duccio Armenise <info@duccioarmenise.it>
2011-05-21 15:26:44 +0200

    Exercises 5.5 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
+	
+	#returns the logo proper image tag
+	def logo
+		logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+	end
 end
